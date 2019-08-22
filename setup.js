1d2608652aead5ebd7bae05597f3d5fcfb5f6917
duncangarde <duncangarde@hotmail.com>
2019-08-22 18:31:51 +0100

    allowing for answering questions

--- setup.js
+++ setup.js
@@ -1,27 +1,10 @@
+function chLoad(version, file_name, element) {
+	chLoadAssets(version);
+	chLoadHtml(element, version, file_name);
+	chLoadTriggers();
+}
 
 function chLoadAssets(version) {
-	// [
-	//   'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js',
-	//   'file:///C:/sites/shelftaught/shelftaught-iframe-js/iframe-load-actions.js',
-	// ].forEach(function(src) {
-	//   if (document.querySelectorAll('[src="' + src + '"]').length > 0) {
-	//   	if (typeof iFrameResize === 'function') {
-	//   		iFrameResize();
-	//   	};
-	//   	if (typeof stReceiveMessage === 'function') {
-	//   		window.addEventListener("message", stReceiveMessage);
-	//   	};
-	//   	if (typeof iFrameLazyLoad === 'function') {
-	//   		iFrameLazyLoad();
-	//   	};
-	//   }
-	//   else {
-	// 	  var script = document.createElement('script');
-	// 	  script.src = src;
-	// 	  script.async = false;
-	// 	  document.head.appendChild(script);
-	//   };
-	// });
 	var css_assets = ['https://use.typekit.net/zzx2vim.css']
 	if (version) {
 		css_assets.push('https://cdn.jsdelivr.net/gh/duncangarde/chath@' + version + '/pest.css')
@@ -73,16 +56,82 @@ function chLoadHtml(setup, version, file_frag) {
     xmlhttp.open("GET", 'https://cdn.jsdelivr.net/gh/duncangarde/chath@' + version + '/' + file_frag + '_headless.html', true);
     xmlhttp.send();
 }
+
+function chhLoadTriggers() {
+	var answers = document.querySelectorAll(".ans-sym");
+	 for (let i = 0; i < answers.length; i++) {
+	     answers[i].addEventListener("click", function() {
+	       chhExpandSibling(this.parentNode.parentNode);
+	       chhPickSelf(this);
+	     });
+	 }
+	var see_alls = document.querySelectorAll(".chh-see-all");
+	 for (let i = 0; i < see_alls.length; i++) {
+	     see_alls[i].addEventListener("click", function() {
+	     	var cont = this.closest('[data-class="chh-qa-section"]');
+	     	var collapseds = cont.querySelectorAll('[data-class="ans-cont"]');
+			 for (let i = 0; i < collapseds.length; i++) {
+			     chhExpandSibling(collapseds[i].parentNode)
+			 };
+	     });
+	 }
+}
+
+function chhAppearSeeAll(el) {
+	var cont = el.closest('[data-class="chh-qa-section"]');
+	cont.querySelector('.chh-see-all').classList.remove('hidden')
+}
+
+function chhDisappearSeeAll(el) {
+	var cont = el.closest('[data-class="chh-qa-section"]');
+	cont.querySelector('.chh-see-all').classList.add('hidden')
+}
+
+function chhPickSelf(el) {
+	var self_active = el.classList.contains('picked');
+	var us = el.parentNode.children;
+	for (i = 0; i < us.length; i++) {
+	  us.item(i).classList.remove('picked');
+	};
+	if (!self_active)
+	{
+		el.classList.add('picked');
+	};
+}
+
+function chhExpandSibling(el) {
+	var parent = el.parentNode;
+	var expandable = el.nextElementSibling;
+	if (expandable && !el.classList.contains('expanded')) 
+	{
+		el.classList.add('expanded');
+		var this_level_height = expandable.scrollHeight
+		var parent_level_height = 0
+		if (expandable.parentNode.classList.contains('expandable')) {
+			parent_level_height = expandable.parentNode.scrollHeight;
+			total_height = this_level_height + parent_level_height
+			expandable.parentNode.style.height = 'auto';
+		}
+		expandable.style.height = this_level_height + 'px';
+		expandable.classList.remove('collapsed');
+	}
+}
+
 		 	
 function chhToggleSiblings(el) {
 	var parent = el.parentNode;
 	var expandable = el.nextElementSibling;
-	if (el.classList.contains('expanded')) {
+	if (el.classList.contains('expanded')) 
+	{
+		chhDisappearSeeAll(el);
 		el.querySelector('.p-symbol').classList.remove('hidden');
 		el.querySelector('.t-symbol').classList.add('hidden');
 		el.classList.remove('expanded');
 		expandable.style.height = "";
-	} else {
+	} 
+	else 
+	{
+		chhAppearSeeAll(el);
 		el.querySelector('.p-symbol').classList.add('hidden');
 		el.querySelector('.t-symbol').classList.remove('hidden');
 		el.classList.add('expanded');
@@ -95,7 +144,6 @@ function chhToggleSiblings(el) {
 		}
 		
 
-		expandable.style.height = this_level_height + 'px';
-
+	expandable.style.height = this_level_height + 'px';
 	}
 }
